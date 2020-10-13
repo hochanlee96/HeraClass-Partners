32235f80a39c48adbdb613714c6f38ff827f9349
HochanLee <j65hcl@gmail.com>
2020-10-13 12:12:49 +0900

    partner can post class

--- src/routes/NewClass.js
+++ src/routes/NewClass.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import NewClassForm from '../components/NewClassForm';
+
 const NewClass = props => {
     return (
         <>
             <div>
                 Add New Class
-        </div>
+            </div>
             <Link to="/">Home</Link>
+            <NewClassForm />
         </>
     )
 }
